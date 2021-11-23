891ba1771322a53b38f0693cea4af48e0fedef3b
maksim <big.maxim@gmai.com>
2021-11-23 19:37:03 +0200

    in7

--- network.tf
+++ network.tf
@@ -65,28 +65,7 @@ resource "azurerm_network_interface_security_group_association" "kursach" {
     network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
 
-    byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
 
 # Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
@@ -102,7 +81,7 @@ output "tls_private_key" {
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "myVM"
     location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    resource_group_name   = "rg-hello-azure-tf"
     network_interface_ids = [azurerm_network_interface.m2terraformnic.id]
     size                  = "Standard_DS1_v2"
 
@@ -132,7 +111,4 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
         storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
 
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
