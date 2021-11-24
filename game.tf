c98e0138f8c6b1a0ccd13673be72f607e36729ff
maksim <big.maxim@gmai.com>
2021-11-24 20:00:04 +0200

    he11

--- game.tf
+++ game.tf
@@ -1,13 +1,4 @@
-# Generate a random integer to create a globally unique name
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-# Create the resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
-  location = "NorthEurope"
-}
+
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "azurerm_resource_group" "rg-hello-azure" {
   location = "NorthEurope"
 }
 
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = "NorthEurope"
+}
