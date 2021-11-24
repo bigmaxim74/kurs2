81a8292e622fc6bb93ec006535d837387dee4b38
maksim <big.maxim@gmai.com>
2021-11-24 20:06:17 +0200

    he13

--- game.tf
+++ game.tf
@@ -1,9 +1,14 @@
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
 
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "webapp-asp-${random_integer.rg-hello-azure-tf.result}"
+  location            = azurerm_resource_group.rg-hello-azure-tf.location
+  resource_group_name = azurerm_resource_group.rg-hello-azure-tf.name
   kind                = "Linux"
   reserved            = true
   sku {
@@ -11,11 +16,12 @@ resource "azurerm_app_service_plan" "appserviceplan" {
     size = "B1"
   }
 }
+
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
 resource "azurerm_app_service" "webapp" {
   name                = "webapp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg-hello-azure-tf.location
+  resource_group_name = azurerm_resource_group.rg-hello-azure-tf.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   source_control {
--- main.tf
+++ main.tf
@@ -6,27 +6,14 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-hello-azure-tf"
-    resource_group_name  = "rg"
     storage_account_name = "sakursazuretf"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
-    resource_random_integer = "ri"
   }
 }
 
 #Create Resource Group
 resource "azurerm_resource_group" "rg-hello-azure" {
   name     = "rg-hello-azure"
-  location = "NorthEurope"
-}
-
-# Generate a random integer to create a globally unique name
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-# Create the resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
   location = "NorthEurope"
 }
